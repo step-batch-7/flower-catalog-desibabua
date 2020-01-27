4f4fc385a8ff4854e5794fc7679924aebbe85fa1
ayush <58028488+desibabua@users.noreply.github.com>
2020-01-27 14:54:40 +0530

    added correct formating feature to the comment on the file db

--- public/js/commentSaver.js
+++ public/js/commentSaver.js
@@ -1,8 +1,16 @@
 const { writeFileSync } = require('fs');
-const dbUrl = `${__dirname}/../../dataBase/commentHistory.json`
+const dbUrl = `${__dirname}/../../dataBase/commentHistory.json`;
 const commentHistory = require(dbUrl);
 
+const changeToCorrectFormate = function(text) {
+  let line = text.replace(/\+/g, ' ');
+  line = line.replace(/%0D%0A/g, '\n');
+  return line;
+};
+
 const saveComment = function(comment) {
+  comment.name = changeToCorrectFormate(comment.name);
+  comment.comment = changeToCorrectFormate(comment.comment);
   comment.date = new Date().toLocaleString();
   commentHistory.unshift(comment);
   writeFileSync(dbUrl, JSON.stringify(commentHistory, null, 2));
